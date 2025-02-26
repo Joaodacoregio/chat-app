c4831f776cc3a9ba91ce4b778de4bbd8cfb6dbc8
joaodacoreg.io <oinaagua6@gmail.com>
2025-02-25 21:11:02 -0300

    att client index

--- Client/obj/Debug/net8.0/chatApp.Client.AssemblyInfo.cs
+++ Client/obj/Debug/net8.0/chatApp.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("chatApp.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23613abb3c624a12067ed2eee1352019b9b99ca5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4748806c825c83c26282e0034eef5c678898a54e")]
 [assembly: System.Reflection.AssemblyProductAttribute("chatApp.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("chatApp.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
