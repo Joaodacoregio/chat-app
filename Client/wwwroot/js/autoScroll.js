1dd755b4a3c51d77d5a2a9c657bf4f9ec8a39428
joaodacoregio <oinaagua6@gmail.com>
2025-03-13 10:07:15 -0300

    auto scroll

---
+++ Client/wwwroot/js/autoScroll.js
@@ -0,0 +1,9 @@
+﻿// wwwroot/scripts/chat.js
+window.scrollToBottom = (element) => {
+    console.log("Elemento recebido:", element);
+    console.log("scrollHeight:", element.scrollHeight);
+    console.log("scrollTop antes:", element.scrollTop);
+    element.scrollTop = element.scrollHeight;
+    console.log("scrollTop depois:", element.scrollTop);
+    return true;
+};
--- Server/obj/Debug/net8.0/chatApp.Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/chatApp.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("chatApp.SErver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf6b2fd348e773a19f8d72e3fb941ff4ee89469b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4c438e46180e2a5a9d1f86f29ff273ef48027f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("chatApp.SErver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("chatApp.SErver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
