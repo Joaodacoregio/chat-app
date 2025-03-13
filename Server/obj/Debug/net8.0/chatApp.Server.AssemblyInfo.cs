bf6b2fd348e773a19f8d72e3fb941ff4ee89469b
joaodacoregio <oinaagua6@gmail.com>
2025-03-13 08:18:55 -0300

    Authentication methods when the user connects

--- Server/obj/Debug/net8.0/chatApp.Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/chatApp.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("chatApp.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82ffa2b41ff91de63bcfd4858f639202539add57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58333ee4797ddb8c890f83b3d295e3bb65035be5")]
 [assembly: System.Reflection.AssemblyProductAttribute("chatApp.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("chatApp.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
